a05e09e80ef743a7c46d64058e00cebceef50445
putraadiuhuyy <putraadipamungkas11@gmail.com>
2025-04-30 08:26:53 +0700

    Add files via upload

---
+++ PROYEK 2/index.js
@@ -0,0 +1,31 @@
+document.querySelectorAll('.menu a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.menu a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
+
